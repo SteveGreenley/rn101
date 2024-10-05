aea3967605484e37a416551d688eb6614cd098c6
SteveGreenley <steve.greenley@gmail.com>
2024-10-05 22:00:45 +1300

    simple layout with flexbox complete, updated roadmap again

--- src/screens/FirstScreen.tsx
+++ src/screens/FirstScreen.tsx
@@ -10,8 +10,19 @@ const FirstScreen = (): JSX.Element => {
   };
   return (
     <View style={styles.container}>
-      <Text>FirstScreen</Text>
-      <Button onPress={goToSecondScreen} title="Go to Second Screen" />
+      <View style={styles.contentCard}>
+        <View style={styles.cardHeader}>
+          <Text style={styles.headerText}>The First Screen Header</Text>
+        </View>
+        <View style={styles.cardBody}>
+          <Text>
+            The First Screen Body. Elit cupidatat eu labore sunt consectetur
+            ullamco amet duis mollit non.{'\n\n'}Amet enim duis ad officia minim
+            elit ad tempor proident laboris deserunt qui excepteur.
+          </Text>
+          <Button onPress={goToSecondScreen} title="Go to Second Screen" />
+        </View>
+      </View>
     </View>
   );
 };
@@ -21,6 +32,24 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  contentCard: {
+    backgroundColor: 'lightgrey',
+    margin: 40,
+    padding: 10,
+    borderRadius: 16,
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  cardHeader: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'grey',
+    padding: 10,
+  },
+  cardBody: {
+    padding: 10,
   },
 });
 
